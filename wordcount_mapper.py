db7fd1272e2f1953c48b1f09b7e92a83e4763c8c
shwetha1205 <64019225+shwetha1205@users.noreply.github.com>
2020-04-22 14:48:39 +0530

    Update wordcount_mapper.py

--- wordcount_mapper.py
+++ wordcount_mapper.py
@@ -1,22 +1,7 @@
-#!/usr/bin/env python   
-#the above just indicates to use python to intepret this file
-
-import sys             #a python module with system functions for this OS
-
-# ------------------------------------------------------------
-#  this 'for loop' will set 'line' to an input line from system 
-# ------------------------------------------------------------
+import sys             
 for line in sys.stdin:  
-#-----------------------------------
-#sys.stdin call 'sys' to read a line from standard input, 
-# note that 'line' is a string object, ie variable, and it has methods that you can apply to it,
-# as in the next line
-# ---------------------------------
-    line = line.strip()  #strip is a method, ie function, associated
-                         #  with string variable, it will strip 
-                         #   the carriage return (by default)
-    keys = line.split()  #split line at blanks (by default), 
-                         #   and return a list of keys
-    for key in keys:     #a for loop through the list of keys
+    line = line.strip()  
+    keys = line.split()  
+    for key in keys:    
         value = 1        
         print('{0}\t{1}'.format(key, value) )
